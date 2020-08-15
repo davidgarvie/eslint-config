25900d9613217b3519ea94318c7c2bf1f4d318bf
David Garvie <garvie88@gmail.com>
2020-08-15 18:56:10 +0100

    feat: update config to support ecma version 8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 8,
     sourceType: "module",
   },
   env: {
